89370dc2483b7ef0c4fdbd996abce638aac227e5
victor-iyiola <javafolabi@gmail.com>
2017-09-23 19:17:38 +0100

    Upate .gitignore and modified features.py

--- features.py
+++ features.py
@@ -1,19 +1,21 @@
 # coding: utf-8
 
 # In[1]:
-import os, sys
+import os
+import sys
+
 import numpy as np
-from tqdm import tqdm
 from PIL import Image
+from tqdm import tqdm
 
 
 # In[2]:
 class Features(object):
-    def __init__(self, data_dir='flower_photos', image_size=150):
+    def __init__(self, data_dir='dataset/flowers', image_size=150):
         self.data_dir = data_dir
         self.image_size = image_size
         self.flowers = [f for f in os.listdir(self.data_dir) if f[0] is not '.']
-        
+
     def create(self, save=True, save_file='datasets.npy', gray=False, flatten=True):
         datasets = []
         for flower in self.flowers:
@@ -29,7 +31,7 @@ class Features(object):
                         img = np.array(img, dtype=np.float32)
                         if flatten:
                             img = img.flatten()
-                        label = self._create_labels(flower)
+                        label = self.__create_labels(flower)
                         datasets.append([img, label])
                     except Exception as e:
                         sys.stderr.write('{}'.format(e))
@@ -41,8 +43,9 @@ class Features(object):
                 os.unlink(save_file)
             np.save(save_file, datasets)
         return datasets
-    
-    def train_test_split(self, datasets, test_size=0.1):
+
+    @staticmethod
+    def train_test_split(datasets, test_size=0.1):
         test_size = int(len(datasets) * test_size)
         train = datasets[:-test_size]
         test = datasets[-test_size:]
@@ -51,8 +54,8 @@ class Features(object):
         test_X = np.array([x[0] for x in test], dtype=np.float32)
         test_y = np.array([x[1] for x in test], dtype=np.float32)
         return np.array([train_X, train_y, test_X, test_y])
-        
-    def _create_labels(self, flower):
+
+    def __create_labels(self, flower):
         labels = np.zeros(len(self.flowers), dtype=np.float32)
         index = self.flowers.index(flower)
         for f in self.flowers:
@@ -60,19 +63,20 @@ class Features(object):
                 labels[index] = 1.
         return labels
 
+
 # In[3]:
 if __name__ == '__main__':
     features = Features()
     datasets = features.create()
     train_X, train_y, test_X, test_y = features.train_test_split(datasets)
-    
+
     print('\nFirst training example:')
     print(train_X[0])
     print(train_y[0])
-    
+
     print('\nFirst testing examples:')
     print(test_X[0])
     print(test_y[0])
-    
-    print('\nLength of training sets:{:,}'.format(len(train_X)))
-    print('Length of testing sets: {:,}\n'.format(len(test_X)))
+
+    print('\nLength of training sets: {:,}'.format(len(train_X)))
+    print('Length of testing sets:  {:,}\n'.format(len(test_X)))
